0f13431ed0ded3a45c206408f702b0db0017c59f
Avan <michael@bizbox.ph>
2026-03-26 19:28:14 +0800

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom'; // 👈 add this
 import App from './App';
 import './index.css';
 import { ThemeContextProvider } from './common/lib/themes/themeContext';
-
 const root = ReactDOM.createRoot(document.getElementById('root')!);
-
 root.render(
   <React.StrictMode>
     <ThemeContextProvider>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <App />
     </ThemeContextProvider>
   </React.StrictMode>
 );
